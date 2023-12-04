ee87e9ac5d67a17f0a384da0aee3b4f30f38b568
2eg4rizve <2eg4rizve.com>
2023-12-04 15:51:41 +0600

    deploy start

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,11 +14,17 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mjrato5.mongodb.net/?retryWrites=true&w=majority`;
 
-const uri =
-  "mongodb+srv://storeDB:hzE7qAl38IuFxjFE@cluster0.mjrato5.mongodb.net/?retryWrites=true&w=majority";
+//storeDB
+//hzE7qAl38IuFxjFE
 
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_PASS);
+// const uri =
+//   "mongodb+srv://storeDB:hzE7qAl38IuFxjFE@cluster0.mjrato5.mongodb.net/?retryWrites=true&w=majority";
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mjrato5.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log("process.env.DB_USER : ",process.env.DB_USER);
+console.log("process.env.DB_PASS : ",process.env.DB_PASS);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -142,9 +149,9 @@ async function run() {
     // Send a ping to confirm a successful connection
     //await client.db("admin").command({ ping: 1 });
 
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
